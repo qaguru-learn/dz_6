75d1e504268ae2ba08899ec20142238d1a51114c
legend23 <yu@vtechnology.ru>
2024-02-20 22:40:00 +0500

    исправил условие (с 22 до 6 часов утра - ночь)

--- homework.py
+++ homework.py
@@ -7,8 +7,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    is_dark_theme = None
-    if time(hour=6) < current_time or current_time >= time(hour=23):
+    if time(hour=6) > current_time or current_time >= time(hour=22):
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -29,9 +28,8 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
     if dark_theme_enabled_by_user is None:
-        if time(hour=6) < current_time or current_time >= time(hour=23):
+        if time(hour=6) > current_time or current_time >= time(hour=22):
             is_dark_theme = True
         else:
             is_dark_theme = False
